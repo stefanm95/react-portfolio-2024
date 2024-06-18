c6bc1eeabdf2c4c3bc96fa16e3c8947efc0344cc
stefanm95 <stefan.moise95@icloud.com>
2024-06-18 11:52:29 +0300

    added Loader and custom hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Process from "./pages/Process";
-import Portfolio from "./pages/Portfolio"
+import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Loader from "./components/Loader";
 
 import "./App.css";
+import useScrollToTop from "./hooks/UseScrollToTop";
 
 function App() {
+  useScrollToTop();
+
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/portfolio" element={<Portfolio />}></Route>
-        <Route exact path="/process" element={<Process />}></Route>
-        <Route exact path="/contact" element={<Contact />}></Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/portfolio" element={<Portfolio />}></Route>
+          <Route exact path="/process" element={<Process />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
